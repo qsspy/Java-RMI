fbea6aa73c125c88fe710bfd233253e9894bc168
Daniel <danel.filipek@gmail.com>
2021-10-25 19:14:31 +0200

    [Client] - Added logic responsible for fetching input from console

--- src/Client.java
+++ src/Client.java
@@ -1,6 +1,10 @@
 public class Client {
 
     public static void main(String[] args) {
-        System.out.println("Client");
+        final MatrixHelper helper = new MatrixHelper(2,2);
+        helper.promptForMatrixInput();
+
+        //for debug
+        helper.showMatrixes();
     }
 }
