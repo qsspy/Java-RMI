d4c13e582c7e0db96aa6edbcb333e5e40e5ac734
qsspy <58231905+qsspy@users.noreply.github.com>
2021-10-26 13:03:49 +0200

    [Client, Server] - Defined interface between client and server

--- src/Client.java
+++ src/Client.java
@@ -1,10 +1,30 @@
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
 public class Client {
 
     public static void main(String[] args) {
         final MatrixHelper helper = new MatrixHelper(2,2);
         helper.promptForMatrixInput();
 
-        //for debug
+
+        //TODO for debug, delete later
         helper.showMatrixes();
+
+        try {
+            // Getting the registry
+            Registry registry = LocateRegistry.getRegistry(Configuration.RMI_REGISTRY_PORT);
+
+            // Looking up the registry for the remote object
+            MatrixMultiplicationService service = (MatrixMultiplicationService) registry.lookup(Configuration.SERVER_SERVICE_IMPL_NAME);
+
+            // Calling the remote method using the obtained object
+            int[][] outputMatrix = service.multiplyMatrixes(helper.getFirstMatrix(), helper.getSecondMatrix());
+            System.out.println("Macierz wynikowa :");
+            helper.printMatrix(outputMatrix);
+
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
     }
 }
---
+++ src/Configuration.java
@@ -0,0 +1,9 @@
+public class Configuration {
+
+    //defines default matrix dimension
+    public static final int DEFAULT_MATRIX_DIMENSION = 2;
+    //defines name of server in RMI registry
+    public static final String SERVER_SERVICE_IMPL_NAME = "server";
+    //defines port of RMI registry on local machine
+    public static final int RMI_REGISTRY_PORT = 1099;
+}
