b3213191100fb9616746a3885bdcf4f860cf120e
Igor Sokół <asus.11@tlen.pl>
2021-10-29 11:41:36 +0200

    [Server, Agents] - Full implementation of operations executions

--- src/MatrixMultiplicationServiceImpl.java
+++ src/MatrixMultiplicationServiceImpl.java
@@ -1,8 +1,84 @@
-public class MatrixMultiplicationServiceImpl implements MatrixMultiplicationService{
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+public class MatrixMultiplicationServiceImpl implements MatrixMultiplicationService {
+
+    CalculationService[] agents;
+    private int agentsInUse;
+
+    public MatrixMultiplicationServiceImpl(int agentsNumber) {
+        this.agentsInUse = agentsNumber;
+        try {
+            Registry registry = LocateRegistry.getRegistry(Configuration.RMI_REGISTRY_PORT);
+            agents = new CalculationService[Configuration.MAX_AGENT_ID];
+            for (int i = 0; i < Configuration.MAX_AGENT_ID; i++) {
+                int agentIndex = i + 1;
+                String[] args = new String[]{
+                        String.valueOf(agentIndex)
+                };
+                Agent.main(args);
+                agents[i] = (CalculationService) registry.lookup("Agent" + agentIndex);
+            }
+        } catch (RemoteException | NotBoundException e) {
+            e.printStackTrace();
+        }
+    }
 
     @Override
     public int[][] multiplyMatrixes(int[][] firstMatrix, int[][] secondMatrix) {
-        //TODO by server
-        return firstMatrix;
+
+        // TODO: Hard-coded. To fix.
+        OperationDto[] multiplications = new OperationDto[]{
+                new OperationDto(firstMatrix[0][0], secondMatrix[0][0], Operator.MULTIPLY),
+                new OperationDto(firstMatrix[0][1], secondMatrix[1][0], Operator.MULTIPLY),
+                new OperationDto(firstMatrix[0][0], secondMatrix[0][1], Operator.MULTIPLY),
+                new OperationDto(firstMatrix[0][1], secondMatrix[1][1], Operator.MULTIPLY),
+                new OperationDto(firstMatrix[1][0], secondMatrix[0][0], Operator.MULTIPLY),
+                new OperationDto(firstMatrix[1][1], secondMatrix[1][0], Operator.MULTIPLY),
+                new OperationDto(firstMatrix[1][0], secondMatrix[0][1], Operator.MULTIPLY),
+                new OperationDto(firstMatrix[1][1], secondMatrix[1][1], Operator.MULTIPLY)
+        };
+
+        int[] multiplicationResults = executeOperations(multiplications);
+
+        OperationDto[] additions = new OperationDto[]{
+                new OperationDto(multiplicationResults[0], multiplicationResults[1], Operator.ADD),
+                new OperationDto(multiplicationResults[2], multiplicationResults[3], Operator.ADD),
+                new OperationDto(multiplicationResults[4], multiplicationResults[5], Operator.ADD),
+                new OperationDto(multiplicationResults[6], multiplicationResults[7], Operator.ADD),
+        };
+        int[] additionsResults = executeOperations(additions);
+
+        return new int[][]{
+                {
+                        additionsResults[0],
+                        additionsResults[1]
+                },
+                {
+                        additionsResults[2],
+                        additionsResults[3]
+                }
+        };
+    }
+
+    private int[] executeOperations(OperationDto[] operations) {
+        Executor[] executors = new Executor[operations.length];
+        int[] results = new int[operations.length];
+        for (int i = 0; i < operations.length; i++) {
+            executors[i] = new Executor(agents[i % agentsInUse], operations[i]);
+            executors[i].start();
+
+            try {
+                executors[i].join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            results[i] = executors[i].getResult();
+        }
+
+        return results;
     }
 }
--- src/Server.java
+++ src/Server.java
@@ -2,16 +2,15 @@ import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
 
-public class Server extends MatrixMultiplicationServiceImpl {
-
-    public Server() {}
+public class Server {
 
     public static void main(String[] args) {
         try {
-            MatrixMultiplicationServiceImpl obj = new MatrixMultiplicationServiceImpl();
+            Registry registry = LocateRegistry.createRegistry(Configuration.RMI_REGISTRY_PORT);
+
+            MatrixMultiplicationServiceImpl obj = new MatrixMultiplicationServiceImpl(Configuration.AGENTS_IN_USE);
             MatrixMultiplicationService service = (MatrixMultiplicationService) UnicastRemoteObject.exportObject(obj, 0);
 
-            Registry registry = LocateRegistry.createRegistry(Configuration.RMI_REGISTRY_PORT);
             registry.bind(Configuration.SERVER_SERVICE_IMPL_NAME, service);
             System.out.println("Server ready");
         } catch (Exception e) {
