0f2a0ebf28eead9fa1ae7b1133a721416bd0664a
Igor Sokół <asus.11@tlen.pl>
2021-11-03 21:53:54 +0100

    Last fixes

--- README.md
+++ README.md
@@ -7,7 +7,7 @@
  1. Go to ```src``` directory by ```cd src``` command.
  2. Type ```start rmiregistry``` in your terminal.
  3. Compile all ```.java``` files for example by ```javac *.java``` command.
- 4. Run ```AgentsRunner.class```. 
+ 4. Run between 1 and 100 agents by using command: ```java Agent 1``` (where ```1``` is Agent's ID). 
  5. Run ```Server.class``` in new terminal.
  6. Run ```Client.class``` in new terminal.
  7. Follow the instructions.
--- src/CalculationServiceImpl.java
+++ src/CalculationServiceImpl.java
@@ -4,29 +4,25 @@ public class CalculationServiceImpl implements CalculationService{
 
     @Override
     synchronized public int calculate(final OperationDto operation) throws RemoteException {
-        //simulatedWaitTime
-        System.out.println("Started Working....");
+        System.out.println("Started Working...");
         try {
-            Thread.sleep(Utils.getRandomLong(Configuration.AGENT_MIN_SLEEP_TIME, Configuration.AGENT_MAX_SLEEP_TIME));
+            Thread.sleep(Utils.getRandomLong(1000, 1000));
         } catch (final InterruptedException exception) {
             exception.printStackTrace();
         }
 
-        //TODO jakoś zmienić
-        System.out.println("Finished Working....");
-
+        int result;
         switch (operation.getOperator()) {
-            case ADD -> {
-                return add(operation.getArg1(), operation.getArg2());
-            }
-            case MULTIPLY -> {
-                return multiply(operation.getArg1(), operation.getArg2());
-            }
+            case ADD -> result = add(operation.getArg1(), operation.getArg2());
+            case MULTIPLY -> result = multiply(operation.getArg1(), operation.getArg2());
             default -> {
                 System.out.println("Wykryto nie wspieraną operację, zwracam 0!");
-                return 0;
+                result = 0;
             }
         }
+
+        System.out.println("Finished Working...");
+        return result;
     }
 
     private int add(final int arg1,final int arg2) {
